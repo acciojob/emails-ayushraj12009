5830d76d4658a1f9f14e4a412429aef279cf2432
ayushraj12009 <ayushraj12009@gmail.com>
2023-09-23 11:32:10 +0530

    some changes

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,11 +1,14 @@
 package com.driver;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Email {
 
     private String emailId;
     private String password;
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -18,12 +21,23 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public boolean changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if (oldPassword.equals(password)) {
+            String regex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[^A-Za-z\\d]).{8,}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(newPassword);
+            this.password = newPassword;
+            return Boolean.parseBoolean(newPassword);
+
+        } else {
+            return false;
+        }
     }
 }
